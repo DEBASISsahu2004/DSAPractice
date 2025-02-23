45e29d0cfc8ee16215448f8b04ee84d3f6bb82e0
Debasis Sahu <sahurocky524@gmail.com>
2025-02-23 12:00:59 +0530

    Time: 4 ms (18.56%) | Memory: 97.3 MB (86.94%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,9 +4,12 @@ public:
         int maxPro = 0;
         int initial = INT_MAX;
         for(int i=0; i<prices.size(); i++){
-            if(prices[i] < initial) initial = prices[i];
-            int dif = prices[i]-initial;
-            if(dif > maxPro) maxPro = dif;
+            // if(prices[i] < initial) initial = prices[i];
+            // int dif = prices[i]-initial;
+            // if(dif > maxPro) maxPro = dif;
+
+            initial = min(prices[i], initial);
+            maxPro = max(maxPro, prices[i]-initial);
         }
 
         return maxPro;
