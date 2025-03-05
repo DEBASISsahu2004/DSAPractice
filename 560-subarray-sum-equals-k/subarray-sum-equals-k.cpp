ad8283261847373b23b74514e463b75d756cfe1a
Debasis Sahu <sahurocky524@gmail.com>
2025-03-05 15:13:28 +0530

    Time: 2067 ms (21.50%) | Memory: 34.2 MB (99.10%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -5,12 +5,15 @@ public:
 
         for (int i = 0; i < nums.size(); i++) {
             int sum = nums[i];
-            if (sum == k)
+            if (sum == k){
                 count++;
+            }
+
             for (int j = i + 1; j < nums.size(); j++) {
                 sum += nums[j];
-                if (sum == k)
+                if (sum == k){
                     count++;
+                }
             }
         }
 
