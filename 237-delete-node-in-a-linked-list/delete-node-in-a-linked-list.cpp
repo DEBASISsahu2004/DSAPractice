df9012d79e7d53fbf691d179c1c6cc905726af2c
Debasis Sahu <sahurocky524@gmail.com>
2025-06-30 12:34:12 +0530

    Time: 10 ms (33.70%) | Memory: 12.5 MB (18.29%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -9,11 +9,11 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        node->val = node->next->val;
         ListNode* temp = node->next;
-        node->next = node->next->next;
+        node->val = temp->val;
+        node->next = temp->next;
 
-        // delete temp;
+        delete temp;
     
     }
 };
