8b65fa1c2b5048a85bb767c9b50cfde29b82a481
Debasis Sahu <sahurocky524@gmail.com>
2025-09-19 11:33:59 +0530

    Time: 15 ms (11.32%) | Memory: 27.1 MB (5.45%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> re;
+        map<int, int> mp;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]]++;
+            if (mp[nums[i]] > n / 3 &&
+                find(re.begin(), re.end(), nums[i]) == re.end()) {
+                re.push_back(nums[i]);
+            }
+        }
+        return re;
+    }
+};
