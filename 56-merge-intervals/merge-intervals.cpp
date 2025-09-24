a21920b00dfbcfec778b28864a9bc42fd0c687dc
Debasis Sahu <sahurocky524@gmail.com>
2025-09-24 13:59:06 +0530

    Time: 8 ms (26.47%) | Memory: 23.9 MB (31.10%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -5,21 +5,42 @@ public:
         sort(intervals.begin(), intervals.end());
         int n = intervals.size();
 
-        for (int i = 0; i < n; i++) {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            if(!re.empty() && end <= re.back()[1]) continue;
-
-            for(int j=i+1; j<n; j++){
-                if(intervals[j][0] <= end){
-                    end = max(end, intervals[j][1]);
-                }else{
-                    break;
-                }
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] <= end) {
+                end = max(end, intervals[i][1]);
+            } else {
+                re.push_back({start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
             }
-            re.push_back({start, end});
         }
+        re.push_back({start, end});
+
         return re;
+
+        // ---> brute force
+
+        // vector<vector<int>> re;
+        // sort(intervals.begin(), intervals.end());
+        // int n = intervals.size();
+
+        // for (int i = 0; i < n; i++) {
+        //     int start = intervals[i][0];
+        //     int end = intervals[i][1];
+
+        //     if(!re.empty() && end <= re.back()[1]) continue;
+
+        //     for(int j=i+1; j<n; j++){
+        //         if(intervals[j][0] <= end){
+        //             end = max(end, intervals[j][1]);
+        //         }else{
+        //             break;
+        //         }
+        //     }
+        //     re.push_back({start, end});
+        // }
+        // return re;
     }
 };
